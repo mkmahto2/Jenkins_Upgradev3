9ac2d7c8f5cba10fc68448a77d3b00c4594c756e
mukesh kumar mahto <mukesh.7755@gmail.com>
2024-07-07 21:45:00 +0530

    Update TestGreeter.java

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -27,7 +27,9 @@ public class TestGreeter {
     assertThat(greeter.greet(someone), containsString(someone));
   }
   
-
+ @Test
+  public void greetShouldIncludeGreetingPhrase() {
+    String someone = "World";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
